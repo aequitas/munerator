ac98d1d9c9c701bef89a1c9a20f02d94e688f060
Johan Bloemberg <mail@ijohan.nl>
2014-03-21 22:55:58 +0100

    Added basic docs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='munerator',
-    version="0.3.17",
+    version="0.3.18",
     description='Manager of OpenArena battles',
     long_description=read("README.rst"),
     author='Johan Bloemberg',
