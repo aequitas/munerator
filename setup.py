8707060e0680c7a66dd24fc3add77fec0107af75
Johan Bloemberg <mail@ijohan.nl>
2014-03-22 16:25:33 +0100

    travis deploy

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='munerator',
-    version="0.3.18",
+    version="0.3.19",
     description='Manager of OpenArena battles',
     long_description=read("README.rst"),
     author='Johan Bloemberg',
