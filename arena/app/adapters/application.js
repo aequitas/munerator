e36852c66d692315e2a20d54e77aaa13d8588203
Johan.Bloemberg <Johan.Bloemberg@spilgames.com>
2014-06-27 12:52:03 +0200

    Fix frontend api caching

--- arena/app/adapters/application.js
+++ arena/app/adapters/application.js
@@ -5,4 +5,10 @@ import DS from "ember-data";
 export default DS.RESTAdapter.extend({
     host: ArenaENV.api_endpoint,
     namespace: 'api/1',
+    ajax: function(url, type, hash) {
+        if (Ember.isEmpty(hash)) hash = {};
+        if (Ember.isEmpty(hash.data)) hash.data = {};
+        hash.data.ts = new Date().getTime();
+        return this._super(url, type, hash);
+    }
 });
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='munerator',
-    version="0.8.5",
+    version="0.8.6",
     description='Manager of OpenArena battles',
     long_description=read("README.rst"),
     author='Johan Bloemberg',
