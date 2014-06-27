a3cf5c17d7734ad23f3d0f575a86235ec4f7ac15
Johan Bloemberg <mail@ijohan.nl>
2014-06-27 23:57:25 +0200

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='munerator',
-    version="0.9.2",
+    version="0.9.3",
     description='Manager of OpenArena battles',
     long_description=read("README.rst"),
     author='Johan Bloemberg',
