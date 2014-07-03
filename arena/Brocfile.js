270f021d7bad729487252df4530bc2098ea6fc1e
Johan Bloemberg <mail@ijohan.nl>
2014-07-04 00:51:20 +0200

    Remove bs_for_ember for now

--- arena/Brocfile.js
+++ arena/Brocfile.js
@@ -16,10 +16,10 @@ app.import('vendor/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/c
 
 // Bootstrap for ember
 // Careful adding additional modules here - some require full handlebars.js, and by default ember-cli only includes runtime in production
-['bs-core.min.js', 'bs-modal.min.js', 'bs-label.min.js', 'bs-button.min.js', 'bs-basic.min.js', 'bs-popover.min.js', 'bs-progressbar.min.js'].forEach(function (path) {
-  var fullPath = 'vendor/ember-addons.bs_for_ember/dist/js/' + path;
-  app.import(fullPath);
-});
+// ['bs-core.min.js', 'bs-modal.min.js', 'bs-label.min.js', 'bs-button.min.js', 'bs-basic.min.js', 'bs-popover.min.js', 'bs-progressbar.min.js'].forEach(function (path) {
+//   var fullPath = 'vendor/ember-addons.bs_for_ember/dist/js/' + path;
+//   app.import(fullPath);
+// });
 
 // Put the bootstrap fonts in the place that the bootstrap css expects to find them.
 var pickFiles = require('broccoli-static-compiler');
